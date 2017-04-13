c6b0585997b85de76f8a943fd5f5625d5ca4a901
Geisibel Ramos <geisibel.03@gmail.com>
2017-04-13 11:09:14 -0300

    Parte Android - Foi adequada as imagens na Tela

--- ProjetoAt/app/src/main/java/com/br/projetoat/MainActivity.java
+++ ProjetoAt/app/src/main/java/com/br/projetoat/MainActivity.java
@@ -1,17 +1,44 @@
 package com.br.projetoat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.activity_main);
+        Button comecar = (Button) findViewById(R.id.comecar);
+        comecar.setOnClickListener(onClickComecar());
     }
+
+    private View.OnClickListener onClickComecar() {
+        return new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                EditText editPin = (EditText) findViewById(R.id.editPin);
+                Integer pin = Integer.parseInt(editPin.getText().toString());
+                Intent intent = new Intent( getContext(),Questionario.class);
+                Bundle params = new Bundle();
+                params.putInt("pin",pin);
+                intent.putExtras(params);
+                startActivity(intent);
+
+
+            }
+        };
+    }
+    private Context getContext(){
+        return this;
+    }
+
 }
--- ProjetoAt/app/src/main/java/com/br/projetoat/Questionario.java
+++ ProjetoAt/app/src/main/java/com/br/projetoat/Questionario.java
@@ -1,7 +1,12 @@
 package com.br.projetoat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Questionario extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class Questionario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionario);
+        Button finalizar = (Button) findViewById(R.id.fim);
+        finalizar.setOnClickListener(onClickFinalizar());
     }
+
+    private View.OnClickListener onClickFinalizar() {
+        return new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //EditText editPin = (EditText) findViewById(R.id.editPin);
+                //Integer pin = Integer.parseInt(editPin.getText().toString());
+                Intent intent = new Intent( getContext(),Finish.class);
+                //Bundle params = new Bundle();
+                //params.putInt("pin",pin);
+                //intent.putExtras(params);
+                //Pegar Notas
+                startActivity(intent);
+
+
+            }
+        };
+    }
+    private Context getContext(){
+        return this;
+    }
+
 }

89e1ac5b245ed8782af6707cca0380fdba86b847
givahits <givahits@gmail.com>
2017-04-21 23:50:44 -0300

    Terminei de fazer o banco de dados e também o dicionário de dados
    Fiz as tabelas e também fiz alguns inserts para testes, fiz também o
    dicionário de dados coloquei em docs e pdf para ficar melhor para fazer
    a programação.
    Está na pasta "Requisitos e Modelagem".

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+gulp.task('stylus', function() {
+	gulp.src('./Site/app/www/src/stylus/style.styl')
+			.pipe(stylus({
+				compress: true
+			}))
+			.pipe(gulp.dest('./Site/app/www/out/css/'))
+})
+
+gulp.task('watch', function() {
+	gulp.watch(['./Site/app/www/src/stylus/**/*.styl'], ['stylus']);
+});

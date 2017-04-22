f276a985add36698866d057b119ab082b47e5a63
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2017-04-22 09:44:28 -0300

    Adicionado base dos If's para Controller

--- Site/app/controllers/autentificacaoController.php
+++ Site/app/controllers/autentificacaoController.php
@@ -6,4 +6,19 @@ $centro = "app/views/autentificacaoView.php";
 if (session_status() !== PHP_SESSION_ACTIVE) {
   session_start();
 }
+$etapa = 1;
+if (isset($_POST['verificar'])) {
+	$etapa = 2;
+
+	unset($_POST);
+}elseif(isset($_POST['enviar'])) {
+	$etapa = 3;
+
+	unset($_POST);
+}elseif(isset($_POST['finalizar'])) {
+	$etapa =1;
+	unset($_POST);
+}
+
+
 ?>

71fdea2343932cd2290620f1c1bf4e151ec018fe
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2017-04-29 11:23:06 -0300

    Adicionado Back Etapa 1 e 2
    Insirir CPF, buscar CPF, cadastro de Clientes e Alteração de Clientes

--- Site/app/views/autentificacaoView.php
+++ Site/app/views/autentificacaoView.php
@@ -1,73 +1,69 @@
 <?php 
 // Página do PIN
 if(isset($etapa) and $etapa == 1) { ?>
-<main>		
-	<div class="panel-simple">
-		<div class="panel-simple__header">
-			<h3 class="panel-simple__title">Aguardando</h3>
-		</div>
-		<div class="panel-simple__body">		
-			<form class="panel-simple__form" method="POST">
-				<label class="panel-simple__label" for="cpf">CPF</label>
-				<input class="panel-simple__input-text" id="cpf" type="text" name="cpf" placeholder="CPF" requerid>
+	<main>		
+		<div class="panel-simple">
+			<div class="panel-simple__header">
+				<h3 class="panel-simple__title">Aguardando</h3>
+			</div>
+			<div class="panel-simple__body">		
+				<form class="panel-simple__form" method="POST">
+					<label class="panel-simple__label" for="cpf">CPF</label>
+					<input class="panel-simple__input-text" id="cpf" type="text" name="cpf" placeholder="CPF" <?= (isset($cpf)) ? 'value="'.$cpf.'"' : '' ;?> requerid>
 
-				<input class="panel-simple__submit" type="submit" name="verificar" value="Avançar">
-			</form>
+					<input class="panel-simple__submit" type="submit" name="verificar" value="Avançar">
+				</form>
+			</div>
 		</div>
-	</div>
-</main>
-
+	</main>
 <?php 
 //Pagina dos Dados
 }elseif(isset($etapa) and $etapa == 2) { ?>
-<main>		
-	<div class="panel-data">
-		<div class="panel-data__header">
-			<h3 class="panel-data__title">Dados do cliente</h3>
+	<main>		
+		<div class="panel-data">
+			<div class="panel-data__header">
+				<h3 class="panel-data__title">Dados do cliente</h3>
+			</div>
+			<div class="panel-data__body">		
+				<form class="panel-data__form" method="POST">				
+					<label class="panel-data__label" for="cpf">CPF</label>
+					<input class="panel-data__input-text" id="cpf" type="text" name="cpf" value="<?=$clientesModel->getCpf()?>" requerid>
+
+					<label class="panel-data__label" for="name">Nome</label>
+					<input class="panel-data__input-text" id="name" type="text" name="nome" value="<?=$clientesModel->getNome()?>" requerid>
+
+					<label class="panel-data__label" for="email">E-mail</label>
+					<input class="panel-data__input-text" id="email" type="email" name="email" value="<?=$clientesModel->getEmail()?>" requerid>
+
+					<label class="panel-data__label" for="tel">Telefone</label>
+					<input class="panel-data__input-text" id="tel" type="text" name="telefone" value="<?=$clientesModel->getTelefone()?>" requerid>
+
+					<label class="panel-data__label" for="ra">RA</label>
+					<input class="panel-data__input-text" id="ra" type="text" name="ra" value="<?=$clientesModel->getRa()?>" requerid>
+
+					<input class="panel-data__submit" type="submit" name="enviar" value="Avançar">
+					<input class="panel-data__voltar" type="submit" name="voltar" value="Finalizar">
+				</form>
+			</div>
 		</div>
-		<div class="panel-data__body">		
-			<form class="panel-data__form" method="POST">				
-				<label class="panel-data__label" for="cpf">CPF</label>
-				<input class="panel-data__input-text" id="cpf" type="text" name="cpf" requerid>
-
-				<label class="panel-data__label" for="name">Nome</label>
-				<input class="panel-data__input-text" id="name" type="text" name="nome" requerid>
-
-				<label class="panel-data__label" for="email">E-mail</label>
-				<input class="panel-data__input-text" id="email" type="email" name="email" requerid>
-
-				<label class="panel-data__label" for="tel">Telefone</label>
-				<input class="panel-data__input-text" id="tel" type="text" name="telefone" requerid>
-
-				<label class="panel-data__label" for="ra">RA</label>
-				<input class="panel-data__input-text" id="ra" type="text" name="ra" requerid>
-
-				<input class="panel-data__submit" type="submit" name="enviar" value="Avançar">
-			</form>
-			<form class="panel-data__form" method="POST">	
-				<input class="panel-data__voltar" type="submit" name="voltar" value="Finalizar">
-			</form>
-		</div>
-	</div>
-</main>
-
+	</main>
 <?php 
 // Página de espera
 }elseif(isset($etapa) and $etapa == 3) { ?>
-<main>
-	<div class="panel-simple">
-		<div class="panel-simple__header">
-			<h3 class="panel-simple__title">PIN</h3>
-		</div>
-		<div class="panel-simple__body">		
-			<em class="panel-simple__input-text">4811</em>
+	<main>
+		<div class="panel-simple">
+			<div class="panel-simple__header">
+				<h3 class="panel-simple__title">PIN</h3>
+			</div>
+			<div class="panel-simple__body">		
+				<em class="panel-simple__input-text">4811</em>
 
-			<form class="panel-data__form" method="POST">	
-				<input class="panel-data__submit" type="submit" name="finalizar">
-			</form>
+				<form class="panel-data__form" method="POST">	
+					<input class="panel-data__submit" type="submit" name="finalizar" value="Finalizar">
+				</form>
+			</div>
 		</div>
-	</div>
-</main>
+	</main>
 <?php
 }
 ?>

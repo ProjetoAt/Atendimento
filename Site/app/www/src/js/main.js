bd37afffa97a96a2c827a0114c6c38bda66916bf
vitebo <vitebo51@gmail.com>
2017-06-01 09:43:30 -0300

    Adicionando bower para gerenciar pacotes

--- Site/app/www/src/js/main.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('app', []);
-
-app.controller('loginController', function($scope) {
-	$scope.user = {name:""}
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,14 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
-var concat = require('gulp-concat');
-
-var js = [
-	'./node_modules/angular/angular.min.js',
-	'./Site/app/www/src/js/main.js'
-]
 
 gulp.task('stylus', function() {
-	gulp.src('./Site/app/www/src/stylus/style.styl')
-			.pipe(stylus({
-				compress: true
-			}))
-			.pipe(gulp.dest('./Site/app/www/out/css/'))
+	gulp.src('./Site/app/www/src/stylus/*.styl')
+		.pipe(stylus({
+			compress: true
+		}))
+		.pipe(gulp.dest('./Site/app/www/out/css/'))
 })
 
-gulp.task('concat', function() {
-	gulp.src(js)
-			.pipe(concat('script.min.js'))
-			.pipe(gulp.dest('./Site/app/www/out/js/'))
-});
-
 gulp.task('watch', function() {
-	gulp.watch(['./Site/app/www/src/stylus/**/*.styl'], ['stylus'])
-	gulp.watch(['./Site/app/www/src/js/*.js'], ['concat'])
+	gulp.watch(['./Site/app/www/src/stylus/**/*.styl', './Site/app/www/src/stylus/*.styl'], ['stylus'])
 });
